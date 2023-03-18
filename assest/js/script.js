5bf4df7448023d04b1a2ca16b08aec1f9f1423c0
MehemmedHajyev <hajyeffmehemmed8@gmail.com>
2023-03-19 03:14:14 +0400

    new-pages

---
+++ assest/js/script.js
@@ -0,0 +1,13 @@
+
+
+
+// let closeButton = document.querySelector('.close-btnn')
+
+// let navList = document.querySelector('.nav-list')
+
+
+// // closeButton.addEventListener('click', () => {
+// //     navList.classList.add('.nav-list-actv')
+
+
+// // })
